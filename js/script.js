00a7ec33cd01fa9b7ff6c684cfa4442fdfe694eb
andrinur13 <andrinur13@gmail.com>
2020-04-03 18:52:14 +0700

    beta.  udah bisa search

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-$('#search-button').on('click', function () {
+function searchMovie() {
+    $('#movie-list').html('');
     $.ajax({
         type: "get",
         url: "http://www.omdbapi.com",
@@ -7,8 +8,42 @@ $('#search-button').on('click', function () {
             's': $('#search-input').val()
         },
         dataType: "json",
-        success: function (response) {
-            console.log(response);
+        success: function (result) {
+            if (result.Response == "True") {
+                let movies = result.Search;
+
+                $.each(movies, function (i, data) {
+                    $('#movie-list').append(`
+                        <div class="card col-md-4 mb-3">
+                            <img src="${data.Poster}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"> ${data.Title} </h5>
+                                <h6 class="card-subtitle mb-2 text-muted"> ${data.Year} </h6>
+                                <h5> <a href="#" class="badge badge-primary">See Details</a> </h5>
+                            </div>
+                        </div>
+                    `);
+                });
+
+                $('#search-input').val('');
+
+            } else {
+                $('#movie-list').html(`
+                    <div class="col">
+                        <h1 class="text-center">Movie Not Found</h1>
+                    </div>
+                `);
+            }
         }
     });
+}
+
+$('#search-button').on('click', function () {
+    searchMovie();
 });
+
+$('#search-input').on('keyup', function (event) {
+    if (event.keyCode == 13) {
+        searchMovie();
+    }
+})
